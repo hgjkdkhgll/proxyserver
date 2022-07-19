2f9bee4446ab82c47e570fe1642131c9b5d5ab4c
dos-proxy <mahmood.hussein.basha@gmail.com>
2022-07-19 16:23:43 +0300

    search for book

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const order_server = "192.168.1.5:3000";
 const catalog_server = "192.168.1.6:3000";
 const http = require("http");
 
+app.get('/search/:category',(req) => {  
+    http.get("http://"+catalog_server+"/books?filter="+req.params.category , res =>respons(res));
+});
+function respons(res){
+    let data = "";
+    res.on("data", chunk => {
+        data += chunk;
+});
+res.on("end", () => {
+    let url = JSON.parse(data);
+    console.log(url);
+});
+}
