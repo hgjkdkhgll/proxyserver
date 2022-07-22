8e26fa789e8d10ddcea0875919592e7e059d3c2c
dos-proxy <mahmood.hussein.basha@gmail.com>
2022-07-22 15:49:37 +0300

    node js response print and port change

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express =require('express');
 const app = express();
-const order_server = "192.168.1.5:3000";
+const http = require('http');
+const order_server = "192.168.1.5:5000";
 const catalog_server = "192.168.1.6";
-const port ="3000";
-const http = require("http");
-const { url } = require('inspector');
+const port ="5000";
 
-app.get('/search/:category',(req) => {  
-    http.get("http://"+catalog_server+"/books?filter="+req.params.category , res => {  
+
+app.get('/search/:category',(req,res1) => {  
+    http.get("http://"+catalog_server+"/books?filter="+req.params.category, res => {  
         
-        console.log(res.statusCode);
-        console.log("\n"); 
+        res1.send(res.statusCode);
+        res1.send("\n"); 
          let data = "";
     res.on("data", chunk => {
         data += chunk;
@@ -18,15 +18,16 @@ app.get('/search/:category',(req) => {
 res.on("end", () => {
     let url = JSON.parse(data);
     console.log(url);
+    res1.send(url);
 });
 });
 });
 
-app.get('/info/:id',(req) => {  
-    http.get("http://"+catalog_server+"/books/"+req.params.id , res => {  
+app.get('/info/:id',(req,res1) => {  
+    get("http://"+catalog_server+"/books/"+req.params.id , res => {  
         
-        console.log(res.statusCode);
-        console.log("\n"); 
+        res1.send(res.statusCode);
+        res1.send("\n"); 
          let data = "";
     res.on("data", chunk => {
         data += chunk;
@@ -34,14 +35,15 @@ app.get('/info/:id',(req) => {
 res.on("end", () => {
     let url = JSON.parse(data);
     console.log(url);
+    res1.send(url);
 });
 });
 });
-app.post('/purchase/:id',(req) => {  
-    http.request({path:"/purchase/"+req.params.id,port:port,host:order_server} , res => {  
+app.post('/purchase/:id',(req,res1) => {  
+    request({method:'POST',path:"/purchase/"+req.params.id,port:port,host:order_server} , res => {  
         
-        console.log(res.statusCode);
-        console.log("\n"); 
+        res1.send(res.statusCode);
+        res1.send("\n"); 
          let data = "";
     res.on("data", chunk => {
         data += chunk;
@@ -49,7 +51,8 @@ app.post('/purchase/:id',(req) => {
 res.on("end", () => {
     let url = JSON.parse(data);
     console.log(url);
+    res1.send(url);
 });
 });
 });
-app.listen(3000);
+app.listen(5000);
