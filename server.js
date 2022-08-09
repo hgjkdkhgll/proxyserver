6fa4dd084095a42af0fac6d540ed0b9b6d90e911
unknown <mahmood.hussein.basha@gmail.com>
2022-08-09 19:34:20 +0300

    replication_handle and order server cache delete

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express =require('express');
 const app = express();
 const http = require('http');
-const order_server = "192.168.175.249:5128";
-const catalog_server = "192.168.56.101:6000";
+const order_server = ["192.168.56.101:6100","192.168.56.101:6101"];
+const catalog_server = ["192.168.56.101:6000","192.168.56.101:6001"];
+let flagc=0;
+let flago=0;
 let search_memory = {}
 let info_memory = {}
 
 app.get('/search/:category',async (req,res1) => {  
     if(search_memory[req.params.category]==null){
-       await http.get("http://"+catalog_server+"/api/books/category/"+req.params.category, res => {  
+       await http.get("http://"+catalog_server[flagc]+"/api/books/category/"+req.params.category, res => {  
  
          let data = "";
     res.on("data", chunk => {
@@ -21,6 +23,11 @@ res.on("end", () => {
     let url = JSON.parse(data);
     console.log(url);
     res1.send(url);
+    if(flagc==0){
+        flagc=1;
+    }else if(flagc==1){
+        flagc=0;
+    }
 });
 });
 }
@@ -33,7 +40,7 @@ else {
 
 app.get('/info/:id',async(req,res1) => {  
     if(info_memory[req.params.id]==null) {
-        await http.get("http://"+catalog_server+"/api/books/"+req.params.id , res => { 
+        await http.get("http://"+catalog_server[flagc]+"/api/books/"+req.params.id , res => { 
         
        
         let data = "";
@@ -46,6 +53,11 @@ app.get('/info/:id',async(req,res1) => {
             let url = JSON.parse(data);
             console.log(url);
             res1.send(url);
+            if(flagc==0){
+                flagc=1;
+            }else if(flagc==1){
+                flagc=0;
+            }
         });
 });
 }
@@ -60,10 +72,21 @@ else{
 app.post('/purchase/:id',(req,res1) => {  
     const url = '/api/purchase/' + req.params.id;
     const axios= require('axios');
-    axios.post('http://'+order_server+url).then((res)=>{
+     axios.post('http://'+order_server[flago]+url).then((res)=>{
+        if(res.status==200){
+            delete info_memory[req.params.id]
+        }
+        if(flago==0){
+            flago=1;
+        }else if(flago==1){
+            flago=0;
+        }
     res1.send(res.data);
 }).catch((err)=>{
-    res1.status(404).json(err.response.data);
+    res1.status(404).json(err.response?.data);
 });
 });
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ', err);
+  });
 app.listen(9000);
